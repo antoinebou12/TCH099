5e3c172864d44d63a1bf490b67cdb71771c6abf4
Antoine Boucher <antoine.bou13@gmail.com>
2024-07-19 03:00:42 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 require_once 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
@@ -11,14 +12,23 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+try {
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+} catch (InvalidPathException $e) {
+    // Handle the case where .env file is not found
+    error_log('No .env file found. Continuing without loading environment variables from .env file.');
+}
 
-$host = $_ENV['DB_HOST'];
-$db   = $_ENV['MYSQL_DATABASE'];
-$user = $_ENV['MYSQL_USER'];
-$pass = $_ENV['MYSQL_PASSWORD'];
+$host = getenv('DB_HOST') ?: getenv('AZURE_MYSQL_HOST');
+$db   = getenv('MYSQL_DATABASE') ?: getenv('AZURE_MYSQL_DBNAME');
+$user = getenv('MYSQL_USER') ?: getenv('AZURE_MYSQL_USERNAME');
+$pass = getenv('MYSQL_PASSWORD') ?: getenv('AZURE_MYSQL_PASSWORD');
 $charset = 'utf8mb4';
+
+if (!$host || !$db || !$user || !$pass) {
+    throw new \Exception('Database connection information is not complete.');
+}
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
@@ -32,4 +42,3 @@ try {
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-?>

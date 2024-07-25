446e193f4958aa665be62a84207e7bc81f13fe43
Antoine Boucher <antoine.bou13@gmail.com>
2024-07-25 11:30:24 -0400

    update the docker and docs

--- config.php
+++ config.php
@@ -58,8 +58,6 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
-    PDO::MYSQL_ATTR_SSL_CA       => '/path/to/ca-cert.pem',
-    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Set to true if you want to verify server certificate
 ];
 
 try {
--- db/3insert.sql
+++ db/3insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO Clients (username, email, password, role)
-VALUES ('user', 'u@u.com', PASSWORD('1234'), 'client');
+VALUES ('user', 'u@u.com', SHA2('1234', 256), 'client');
